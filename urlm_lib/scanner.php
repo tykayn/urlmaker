e1393a09bdc7f979cff844a2dd6b3a22b27a073a
tykayn <contact@artlemoine.com>
2015-11-08 22:03:55 +0100

    split langs

--- urlm_lib/scanner.php
+++ urlm_lib/scanner.php
@@ -51,61 +51,12 @@ if ($dir_unfound != 1) {
 
 //HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____HTML_____
                     if ($_POST["langage"] == 'html') {
-                        $langage = 'html';
-                        $prehtml = '<quote>';
-                        $posthtml = '</quote>';
 
-                        if (isset($_POST["backreturn"]) && $_POST["backreturn"] == 1) {
-                            $br = '<br/>';
-                        }
-                        if (isset($_POST["activer"]) && $_POST["activer"] == '1') {
-                            $spe_dim = 1;
-                            $largeur = 'width="' . $_POST["largeur"] . '"';
-                            $hauteur = 'height="' . $_POST["hauteur"] . '"';
-                        }
-                        if (file_exists($pathnormal . '/g/' . $v)) {
-                            $prelien = '<a href="' . $path . '/g/' . $v . '">';
-                            $postlien = '</a>';
-                        }
-                        $add = '<span class="grand">' . htmlspecialchars($prelien) . '</span><span class="thumbing">' . htmlspecialchars('<img src="' . $path . '/' . $v . '" alt="' . $alt . '" title="' . $alt . '"' . $largeur . ' ' . $hauteur . ' />' . $postlien . $br) . '</span><br/>';
-                        $txtpropre = htmlspecialchars($prelien) . htmlspecialchars('<img src="' . $path . '/' . $v . '" alt="' . $alt . '" title="' . $alt . '"' . $largeur . ' ' . $hauteur . ' />' . $postlien . $br) . ' ';
-
+                        require('scans/html.php');
                     } else if ($_POST["langage"] == 'bbcode') {
 
 //BBCODE_____//BBCODE_____//BBCODE_____//BBCODE_____//BBCODE_____//BBCODE_____//BBCODE_____//BBCODE_____//BBCODE_____
-
-                        $langage = 'bbcode';
-                        if (isset($_POST["backreturn"]) && $_POST["backreturn"] == 1) {
-                            $br = '<br/>';
-                        }
-                        //pour lien vers petit ou grand. test du dossier G, puis du dossier THUMB
-                        if ($thumb == 1) {
-                            _log("<br/>test de $pathnormal.'/g/'.$v puis de $pathnormal.'/g/'.$v ");
-                            if (file_exists($pathnormal . '/g/' . $v)) {
-                                $prethumb = '[';
-                                $afterthumb = '|' . $path . '/g/' . $v . ']';
-                                $add = '[url=<span class="grand">' . $path . '/g/' . $v . '</span>][img]<span class="thumbimg">' . $path . '/' . $v . '</span>[/img][/url]' . $br;
-                                $txtpropre = '[url=' . $pathnormal . '/g/' . $v . '][img]' . $path . '/' . $v . '[/img][/url]' . $br;
-
-                            } elseif (file_exists($pathnormal . '/thumb/' . $v)) {
-                                $add = '[url=<span class="grand">' . $pathnormal . '/' . $v . '</span>][img]<span class="thumbimg">' . $path . '/thumb/' . $v . '</span>[/img][/url]' . $br;
-                                $txtpropre = '[url=' . $pathnormal . '/' . $v . '][img]' . $path . '/thumb/' . $v . '[/img][/url]' . $br;
-                            } else {
-                                $add = '[img]<span class="unfound">' . $path . '/' . $v . '</span>[/img]' . $br;
-                                $txtpropre = '[img]' . $path . '/' . $v . '[/img]' . $br;
-                                $pasfound = 1;
-                            }
-
-                        } else {
-                            _log("<br/>dossier ");
-                            $prethumb = '<span class="unfound">';
-                            $afterthumb = '</span>';
-                            $add = '' . $prethumb . '((<span class="thumbimg">' . $path . '/' . $v . '</span>|' . $alt . '|C))' . $afterthumb . $br . '<br/>';
-                            $txtpropre = '((' . $path . '/' . $v . '|' . $alt . '|C))' . $br;
-
-                            $pasfound = 1;
-                        }
-
+                        require('scans/bbcode.php');
 
                     }
 
--- urlm_lib/scans/wiki.php
+++ urlm_lib/scans/wiki.php
@@ -1,28 +1,31 @@
 <?php
-if (isset($_POST["backreturn"]) && $_POST["backreturn"] == 1) {
+if ( isset( $_POST[ "backreturn" ] ) && $_POST[ "backreturn" ] == 1 ) {
 	$br = '%%%';
 }
 //pour lien vers petit ou grand. test du dossier G, puis du dossier THUMB
-if ($thumb == 1) {
-	_log("<br/>test de $pathnormal.'/g/'.$v puis de $pathnormal.'/thumb/'.$v ");
-	if (file_exists($pathnormal . '/g/' . $v)) {
+if ( $thumb == 1 ) {
+	_log( "<br/>test de $pathnormal.'/g/'.$v puis de $pathnormal.'/thumb/'.$v " );
+	if ( file_exists( $pathnormal . '/g/' . $v ) ) {
 		$prethumb = '[';
 		$afterthumb = '|' . $path . '/g/' . $v . ']';
 		$add = '' . $prethumb . '((<span class="thumbimg">' . $path . '/' . $v . '</span>|' . $alt . '|C))<span class="grand">' . $afterthumb . '</span>' . $br . '<br/>';
 
 
-	} elseif (file_exists($pathnormal . '/thumb/' . $v)) {
-		$add = '[((<span class="thumbimg">' . $path . htmlspecialchars($_POST['path']) . '/thumb/' . $v . '</span>|' . $alt . '|C))|<span class="grand">' . $path . '/' . $v . '</span>]' . $br . '<br/>';
+	}
+	elseif ( file_exists( $pathnormal . '/thumb/' . $v ) ) {
+		$add = '[((<span class="thumbimg">' . $path . htmlspecialchars( $_POST[ 'path' ] ) . '/thumb/' . $v . '</span>|' . $alt . '|C))|<span class="grand">' . $path . '/' . $v . '</span>]' . $br . '<br/>';
 		$txtpropre = '[((' . $path . '/' . $dactuel . '/thumb/' . $v . '|' . $alt . '|C))|' . $path . '/' . $dactuel . '/' . $v . ']' . $br . '';
-	} else {
+	}
+	else {
 		$add = '<span class="unfound">((<span class="thumbimg">' . $path . '/' . $dactuel . '/' . $v . '</span>|' . $alt . '|C))' . $br . '</span><br/>';
 		$txtpropre = '((' . $path . '/' . $dactuel . '/' . $v . '|' . $alt . '|C))' . $br . '';
 		$pasfound = 1;
 
 	}
 
-} else {
-	_log("<br/>dossier ");
+}
+else {
+	_log( "<br/>dossier " );
 	$prethumb = '<span class="unfound">';
 	$afterthumb = '</span>';
 	$add = '' . $prethumb . '((<span class="thumbimg">' . $path . '/' . $v . '</span>|' . $alt . '|C))' . $afterthumb . $br . '<br/>';
@@ -30,7 +33,7 @@ if ($thumb == 1) {
 	$pasfound = 1;
 }
 //aperçu d'image pour un dossier à la première image qui passe
-if ($prem_img == 0 && preg_match("#jpg|png#i", $v)) {
+if ( $prem_img == 0 && preg_match( "#jpg|png#i" , $v ) ) {
 	$imgurl = $path . '/' . $dactuel . '/' . $v;
 	$add = '<img src="' . $imgurl . '" class="mini_img responsive-image" style="width: 100%; max-width:100%;" alt="' . $imgurl . '" >  <br/>
                                                            ' . $add;
